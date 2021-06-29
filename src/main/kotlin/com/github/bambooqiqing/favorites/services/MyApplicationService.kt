aa1217bf8845a25ed37de8c1b9d5db379f034fad
GitHub Action <action@github.com>
2021-06-29 06:19:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/bambooqiqing/favorites/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bambooqiqing/favorites/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bambooqiqing.favorites.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bambooqiqing.favorites.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/bambooqiqing/favorites/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bambooqiqing/favorites/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bambooqiqing.favorites.services
 
+import com.github.bambooqiqing.favorites.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
